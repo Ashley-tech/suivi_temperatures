16546845afec0e93da340ba47defb9b95e1f82a6
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-06-16 22:35:42 +0200

    Add configuration, database setup, and initial models for FastAPI application

--- suivi_temperatures_back/app/main.py
+++ suivi_temperatures_back/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from app.config import settings
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {
-        "message": "Bienvenue sur mon API FastAPI"
+        "message": "Bienvenue sur mon API FastAPI",
+        "db": settings.POSTGRES_DB,
+        "host": settings.POSTGRES_HOST,
+        "port": settings.POSTGRES_PORT
     }
